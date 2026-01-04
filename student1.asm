406a51150deecb07ef73424fb80b7fbccf36fe42
biancanodis <bianca.nodis@gmail.com>
2026-01-04 17:21:15 +0200

    Am scris mai multe comentarii
    Updated comments for clarity and improved structure in the assembly code.

--- student1.asm
+++ student1.asm
@@ -1,29 +1,32 @@
 assume cs:code, ds:data
 
+; SEGMENTUL DE DATE
 data segment
 
-    ; mesaj afisat utilizatorului
+    ; Mesaj afisat utilizatorului inainte de citire
+    ; '$' este obligatoriu pentru afisarea cu INT 21h / AH=09h
     mesajInput db 'Introduceti octeti hex (8-16 valori): $'
 
-    ; buffer DOS pentru citire cu INT 21h / AH = 0Ah
-    ; buffer[0] = numar maxim caractere
-    ; buffer[1] = numar caractere citite
-    ; buffer[2...] = caracterele introduse
-    buffer db 50
-           db ?
-           db 50 dup(?)
+    ; Buffer DOS folosit pentru citire cu INT 21h / AH=0Ah
+    ; Structura bufferului:
+    ; buffer[0] = numarul maxim de caractere acceptate
+    ; buffer[1] = numarul efectiv de caractere citite
+    ; buffer[2...] = caracterele introduse de utilizator
+    buffer db 50          ; maxim 50 caractere
+           db ?           ; aici DOS va pune nr. de caractere citite
+           db 50 dup(?)   ; zona unde se vor stoca caracterele
 
-    ; sirul in care stocam octetii convertiti (max 16)
+    ; Vectorul unde vom stoca octetii convertiti din HEX in BINAR
+    ; Maxim 16 octeti conform cerinței
     sirOcteti db 16 dup(?)
 
-    ; lungimea sirului (numarul de octeti cititi)
+    ; Variabila in care memoram lungimea reala a sirului
+    ; (numărul de octeti introdusi)
     l db 0
 
 data ends
 
-
 ; PROCEDURI DEFINITE IN ALTE FISIERE 
-
 extern functions:proc                   
 extern sortare_sir:proc                 
 extern afisare_sir_sortat:proc
@@ -31,117 +34,142 @@ extern calculare_numar_maxim_octetii1:proc
 extern afisare_octet_bit_1_maxim:proc
 
 
-; SEGMENTUL DE COD – MAIN
-
+; SEGMENTUL DE COD
 code segment
 
+; PUNCTUL DE INTRARE AL PROGRAMULUI
 start:
-    ; initializam segmentul de date
+
+    ; Initializăm registrul DS cu adresa segmentului de date
+    ; Făra asta, nu putem accesa variabilele declarate in data segment
     mov ax, data
     mov ds, ax
 
-    ; Afisam mesajul de introducere
-    mov ah, 09h
-    mov dx, OFFSET mesajInput
-    int 21h
+    ; AFISAREA MESAJULUI DE INTRODUCERE
+    mov ah, 09h               ; functia DOS pentru afisare sir
+    mov dx, OFFSET mesajInput ; DX = adresa mesajului
+    int 21h                   ; apel intrerupere DOS
 
-    ; Citim sirul de caractere de la tastatura
-    ; folosind INT 21h / AH = 0Ah
-    mov ah, 0Ah
-    mov dx, OFFSET buffer
-    int 21h
 
-    ; Pregatire conversie HEX -> BINAR
-    ; SI -> primul caracter din buffer
-    ; DI -> unde salvam octetii convertiti
-    ; CL -> numar caractere citite
-    ; BX -> contor octeti
-    
+    ; CITIREA DE LA TASTATURĂ
+    ; Utilizatorul introduce valori HEX separate prin spatiu
+    mov ah, 0Ah               ; funcția DOS pentru citire cu buffer
+    mov dx, OFFSET buffer     ; DX = adresa bufferului
+    int 21h                   ; DOS completeaza bufferul
+
+
+    ; PREGATIREA CONVERSIEI HEX -> BINAR
+
+    ; SI va indica pozitia curenta in buffer
+    ; Sarim peste primele 2 pozitii (lungime max + nr caractere)
     mov si, OFFSET buffer
-    add si, 2                 ; sarim peste primele 2 pozitii din buffer
+    add si, 2
 
+    ; DI va indica pozitia unde salvam octetii convertiti
     mov di, OFFSET sirOcteti
+
+    ; CL = numarul de caractere citite
     mov cl, buffer[1]
-    xor bx, bx                ; bx = 0
+
+    ; BX va fi contorul de octeti convertiti
+    xor bx, bx                ; BX = 0
+
 
 ; BUCLE DE CONVERSIE ASCII HEX -> OCTET BINAR
-
 convert:
-    ; daca nu mai avem caractere, iesim
+
+    ; Daca nu mai sunt caractere de procesat, iesim
     cmp cl, 0
     je done
 
-    ; luam caracterul curent
+    ; Citim caracterul curent din buffer
     mov al, [si]
 
-    ; daca este spatiu, il sarim
+    ; Daca este spatiu, il ignoram
     cmp al, ' '
     je skip
 
-    ; conversie primul caracter hex (HIGH nibble)
+   
+    ; CONVERSIE PRIMUL CARACTER HEX 
 
-    call hexToNibble          ; AL = valoare 0..15
-    shl al, 4                 ; mutam pe nibble-ul superior
-    mov ah, al                ; salvam temporar in AH
+    ; Convertim caracterul ASCII intr-o valoare 0..15
+    call hexToNibble
 
-    inc si                    ; trecem la urmatorul caracter
+    ; Mutam valoarea pe bitii superiori (biții 7..4)
+    shl al, 4
+
+    ; Salvam temporar rezultatul în AH
+    mov ah, al
+
+    ; Trecem la urmatorul caracter
+    inc si
     dec cl
 
-  
-    ; conversie al doilea caracter hex (LOW nibble)
-   
-    mov al, [si]
-    call hexToNibble
-    or ah, al                 ; combinam cei doi nibble
 
-   
-    ; salvam octetul rezultat in sir
-   
-    mov [di], ah
-    inc di                    ; urmatoarea pozitie in sir
-    inc bx                    ; incrementam numarul de octeti
+    ; CONVERSIE AL DOILEA CARACTER HEX 
+
+    mov al, [si]              ; citim al doilea caracter
+    call hexToNibble          ; il convertim în 0..15
+
+    ; Combinam bitii superiori cu cei inferiori
+    or ah, al
 
 
-; Sarim peste spatii
+    ; SALVAREA OCTETULUI FINAL
+
+    mov [di], ah              ; salvam octetul în sirOcteti
+    inc di                    ; trecem la următoarea pozitie
+    inc bx                    ; incrementam contorul de octeti
+
+
+; SARIM PESTE SPATII ȘI CONTINUAM
 
 skip:
     inc si
     dec cl
     jmp convert
 
+
 ; FINAL CONVERSIE
 
 done:
-    ; salvam lungimea sirului
+
+    ; Salvam lungimea sirului (numarul de octeti)
     mov l, bl
 
-    ; pregatim registrele 
+    ; Pregatim registrele
     ; SI = adresa sirului
     ; CX = lungimea sirului
     mov si, OFFSET sirOcteti
     mov cx, bx
 
-    ; Apel proceduri 
-    
+
+    ; APELAREA PROCEDURILOR COLEGILOR
+
     call functions
     call sortare_sir
     call afisare_sir_sortat
     call calculare_numar_maxim_octetii1
     call afisare_octet_bit_1_maxim
 
-    ; Terminarea programului
+
+    ; TERMINAREA PROGRAMULUI
+
     mov ah, 4Ch
     int 21h
 
-
-; SUBRUTINA: conversie caracter HEX -> valoare 0..15
+; SUBRUTINA hexToNibble
+; Converteste:
+; '0'..'9' -> 0..9
+; 'A'..'F' -> 10..15
 
 hexToNibble proc
-    ; daca este cifra '0'..'9'
+
+    ; Daca caracterul este mai mic sau egal cu '9'
     cmp al, '9'
     jle digit
 
-    ; daca este litera 'A'..'F'
+    ; Altfel este litera hexazecimala
     sub al, 'A'
     add al, 10
     ret
@@ -149,6 +177,7 @@ hexToNibble proc
 digit:
     sub al, '0'
     ret
+
 hexToNibble endp
 
 
